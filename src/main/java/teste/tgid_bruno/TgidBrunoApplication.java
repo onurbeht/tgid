b4f6a4570f644434c190f2437b1529f2afdbcc52
onurbeht <brunnobatista25@gmail.com>
2024-08-31 11:23:24 -0300

    First commit

---
+++ src/main/java/teste/tgid_bruno/TgidBrunoApplication.java
@@ -0,0 +1,13 @@
+package teste.tgid_bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgidBrunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgidBrunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste/tgid_bruno/TgidBrunoApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.tgid_bruno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgidBrunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
